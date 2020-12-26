1fdd82fc66f07f673d9e25becc096948abe255ad
kagerw <kagerw093@gmail.com>
2020-12-26 19:46:13 +0900

    firstcommit

---
+++ covid19_form/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace covid19_form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtIso = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(32, 42);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(605, 300);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(561, 393);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtIso
+            // 
+            this.txtIso.Location = new System.Drawing.Point(435, 393);
+            this.txtIso.Name = "txtIso";
+            this.txtIso.Size = new System.Drawing.Size(100, 19);
+            this.txtIso.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 450);
+            this.Controls.Add(this.txtIso);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtIso;
+    }
+}
+
---
+++ covid19_form/Form1.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace covid19_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var iso = txtIso.Text;
+            var client = new RestClient($"https://webhooks.mongodb-stitch.com/api/client/v2.0/app/covid-19-qppza/service/REST-API/incoming_webhook/global?country_iso3={iso}&hide_fields=_id, country, country_code, country_iso2, country_iso3, loc, state, uid");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+
+            //Console.WriteLine(response.Content);
+
+            //var json = JsonSerializer.Deserialize<Root>(response.Content);
+            var json = (JArray)JsonConvert.DeserializeObject(response.Content);
+
+            var item1 = json.Select(item => DateTime.Parse(item["date"].ToString().Substring(0, 10).Replace("-", ""))).Cast<DateTime>().ToArray();
+            var item2 = json.Select(item => (int)item["confirmed_daily"]).Cast<int>().ToArray();
+
+            Plot(item1, item2);
+        }
+
+        private void Plot(DateTime[] dates, int[] values)
+        {
+            chart1.Series.Clear();
+            chart1.ChartAreas.Clear();
+
+            var chartArea1 = "Area1";
+            chart1.ChartAreas.Add(new System.Windows.Forms.DataVisualization.Charting.ChartArea(chartArea1));
+            var legentd1 = "Graph1";
+            chart1.Series.Add(legentd1);
+            chart1.Series[legentd1].ChartType = SeriesChartType.Line;
+
+            for(int i =0;i< dates.Length;i++)
+            {
+                //chart1.Series[legentd1].Points.AddY(values[i]);
+                chart1.Series[legentd1].Points.AddXY(dates[i], Math.Max(0,values[i]));
+            }
+        }
+    }
+}
