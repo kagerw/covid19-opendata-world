3d43bf918c8f4aa8d612173e5fa0abf6f4c85456
kagerw <kagerw093@gmail.com>
2020-12-31 18:01:50 +0900

    実効再生産数の追加

--- covid19_form/Form1.Designer.cs
+++ covid19_form/Form1.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.button1 = new System.Windows.Forms.Button();
             this.cbFunction = new System.Windows.Forms.ComboBox();
@@ -40,16 +40,16 @@
             // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
             this.chart1.Location = new System.Drawing.Point(32, 42);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
             this.chart1.Size = new System.Drawing.Size(605, 300);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
@@ -60,7 +60,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "描画";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
--- covid19_form/Form1.cs
+++ covid19_form/Form1.cs
@@ -23,8 +23,10 @@ namespace covid19_form
             cbFunction.Items.Add("新規発生");
             cbFunction.Items.Add("現在患者");
             cbFunction.Items.Add("回復");
+            cbFunction.Items.Add("移動平均");
 
-            cboIso.DataSource = GetMetaDatas();
+            cboIso.DataSource = GetMetaDatas("countries");
+            //cboReason.DataSource = GetMetaDatas("states");
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -32,10 +34,10 @@ namespace covid19_form
 
             var country = cboIso.SelectedValue.ToString();
 
-            var client = new RestClient($"https://webhooks.mongodb-stitch.com/api/client/v2.0/app/covid-19-qppza/service/REST-API/incoming_webhook/global_and_us?country={country}&min_date=2020-01-01T00:00:00.000Z&hide_fields=_id, country, country_code, country_iso2, country_iso3, loc, state");
+            var client = new RestClient($"https://webhooks.mongodb-stitch.com/api/client/v2.0/app/covid-19-qppza/service/REST-API/incoming_webhook/countries_summary?country={country}&min_date=2020-01-01T00:00:00.000Z&hide_fields=_id, country, country_code, country_iso2, country_iso3, loc, state");
             client.Timeout = -1;
             var request = new RestRequest(Method.GET);
-            IRestResponse response = client.Execute(request);
+            IRestResponse response = client.ExecuteAsync(request).Result;
 
 
             //Console.WriteLine(response.Content);
@@ -46,59 +48,121 @@ namespace covid19_form
                 var json = ConvertToJson<JArray>(response.Content);
 
                 var item1 = json.Select(item => DateTime.Parse(item["date"].ToString().Substring(0, 10).Replace("-", ""))).Cast<DateTime>().ToArray();
-                var item2 = SelectedFunction(json)?.Invoke(json);
+                var item2 = SelectedFunction(json);
 
                 if (item2 != null)
-                    Plot(item1, item2.ToArray());
+                {
+                    ClearPlotArea();
+                    Plot(cbFunction.SelectedItem.ToString(), item1.Skip(7).ToArray() , item2.Cast<double>().ToArray());
+                    //Plot("移動平均",item1, MovingAverage(json.Select(item => (int)item["confirmed_daily"]).ToList(), 7));
+                    Plot("実効再生産数", item1.Skip(7).ToArray(), Rt(json.Select(item => (int)item["confirmed_daily"]).ToList(), 7));
+                }
             }
         }
 
         TResult ConvertToJson<TResult>(string rowData)=> (TResult)JsonConvert.DeserializeObject(rowData);
 
 
-        string[] GetMetaDatas()
+        string[] GetMetaDatas(string tag)
         {
             var client = new RestClient("https://webhooks.mongodb-stitch.com/api/client/v2.0/app/covid-19-qppza/service/REST-API/incoming_webhook/metadata");
             client.Timeout = -1;
             var request = new RestRequest(Method.GET);
             IRestResponse response = client.Execute(request);
-            return ConvertToJson<JToken>(response.Content)["countries"].Select(item => item.ToString()).ToArray();
+            return ConvertToJson<JToken>(response.Content)[tag].Select(item => item.ToString()).ToArray();
         }
 
-        calclationFunc SelectedFunction(JArray json)
+        double[] SelectedFunction(JArray json)
         {
             switch(cbFunction.SelectedIndex)
             {
                 case 0:
                     //新規発生
-                    return array => json?.Select(item => (int)item["confirmed_daily"])?.ToArray();
+                    return json?.Select(item => (double)item["confirmed_daily"])?.ToArray();
                 case 1:
                     //現在入院患者
-                    return array => json?.Select(item => (int)item["confirmed"] - (int)item["recovered"] - (int)item["deaths"]).ToArray();
+                    return json?.Select(item => (double)item["confirmed"] - (int)item["recovered"] - (int)item["deaths"]).ToArray();
                 case 2:
                     //回復
-                    return array => json?.Select(item => (int)item["recovered_daily"]).ToArray();
+                    return json?.Select(item => (double)item["recovered_daily"]).ToArray();
+                case 3:
+                    //移動平均
+                    return MovingAverage(json.Select(item => (double)item["confirmed_daily"]).ToList(), 7);
 
             }
             return null;
+
+
+        }
+        /// <summary>
+        /// 移動平均の計算
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        double[] MovingAverage(
+            IList<double> data,
+            int num)
+        {
+            return Enumerable.Range(0, data.Count() - num)
+                .Select(i => (double)data.Skip(i - num).Take(num).Average())
+                .ToArray();
         }
 
-        private void Plot(DateTime[] dates, int[] values)
+        /// <summary>
+        /// 実効再生産数の計算
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        double[] Rt(
+            IList<int> data,
+            int num)
         {
-            chart1.Series.Clear();
-            chart1.ChartAreas.Clear();
+            return Enumerable.Range(0, data.Count() - num)
+                .Select(i => {
+                    if (i < num)
+                        return 1;
 
-            var chartArea1 = "Area1";
-            chart1.ChartAreas.Add(new System.Windows.Forms.DataVisualization.Charting.ChartArea(chartArea1));
-            var legentd1 = "Graph1";
+                    return Math.Pow((double)data.Skip(i).Take(num).Sum() / Math.Max(0.1, data.Skip(i - num).Take(num).Sum()), (double)5 / 7);
+                    })
+                .ToArray();
+        }
+
+        private void Plot(string areaName, DateTime[] dates, double[] values)
+        {
+            var chartArea1 = "test";
+            if(!chart1.ChartAreas.Any(area => area.Name == chartArea1 ))
+                chart1.ChartAreas.Add(new ChartArea(chartArea1));
+            //if(areaName == "実効再生産数")
+            //    chart1.ChartAreas.Add(new ChartArea(chartArea1)
+            //    {
+            //        Y
+            //    });
+            var legentd1 = areaName;
             chart1.Series.Add(legentd1);
             chart1.Series[legentd1].ChartType = SeriesChartType.Line;
 
-            for(int i =0;i< dates.Length;i++)
+            if (areaName == "実効再生産数")
+            {
+                chart1.Series[legentd1].YAxisType = AxisType.Secondary;
+                chart1.ChartAreas.First().AxisY2.Maximum = 3;
+            }
+
+            for(int i =0;i< values.Length;i++)
             {
                 //chart1.Series[legentd1].Points.AddY(values[i]);
                 chart1.Series[legentd1].Points.AddXY(dates[i], Math.Max(0,values[i]));
             }
+            chart1.Serializer.Save($"{areaName}.txt");
+            chart1.Update();
         }
+
+        private void ClearPlotArea()
+        {
+            chart1.Series.Clear();
+            chart1.ChartAreas.Clear();
+        }
+
     }
 }
